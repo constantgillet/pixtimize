237c0b9983724ca00dedc3792c9b9bfb97defec8
constant <constant@immortal.game>
2024-07-10 13:49:33 +0200

    add get file form s3 function

--- src/libs/s3.ts
+++ src/libs/s3.ts
@@ -1,4 +1,4 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { environment } from "./environment";
 
 const s3 = new S3Client({
@@ -10,3 +10,19 @@ const s3 = new S3Client({
 		secretAccessKey: environment().S3_SECRET_KEY,
 	},
 });
+
+/**
+ * Get a file from the S3 server based on the key
+ * @param key key of the file, the path of the file in the S3 server
+ * @returns
+ */
+export const getFile = async (key: string) => {
+	const params = {
+		Bucket: environment().S3_BUCKET,
+		Key: key,
+	};
+
+	const data = await s3.send(new GetObjectCommand(params));
+
+	return data;
+};
