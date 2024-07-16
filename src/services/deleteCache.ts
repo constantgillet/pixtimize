93ee59545887150f3c7f110415ca20c00cf7ad51
constant <constant@immortal.game>
2024-07-16 13:24:57 +0200

    Add delete cache

--- src/services/deleteCache.ts
+++ src/services/deleteCache.ts
@@ -1,3 +1,20 @@
+import { deleteKeys } from "@/libs/redis";
+import { deleteFolder } from "@/libs/s3";
+
 export const deleteCache = async () => {
-	console.log("Deleting cache");
+	try {
+		//Delete s3 folder
+		deleteFolder("cached/");
+	} catch (error) {
+		console.error("Error deleting cache", error);
+		throw new Error("Error deleting cache");
+	}
+
+	try {
+		//Delete keys in redis
+		deleteKeys("cache:*");
+	} catch (error) {
+		console.error("Error deleting redis cache", error);
+		throw new Error("Error deleting redis cache");
+	}
 };
