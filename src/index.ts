505657cf159c8ad52eef760e911869d524e563dc
constantgillet <cpcdgillet@gmail.com>
2024-08-19 10:06:13 +0200

    add delete cache route

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,26 @@ import { deleteCache } from "@/services/deleteCache";
 import { renderImage } from "@/services/renderImage";
 
 const app = new Elysia()
-  .use(
-    cron({
-      name: "delete-cache",
-      pattern: environment().CACHE_DELETE_CRON, //Every Monday at 1:00
-      run() {
-        deleteCache();
-      },
-    })
-  )
-  .get("/", () => {
-    //Return 200 OK
-    return new Response("OK", { status: 200 });
-  })
-  .get("/*", renderImage)
-  .listen(environment().PORT);
+	.use(
+		cron({
+			name: "delete-cache",
+			pattern: environment().CACHE_DELETE_CRON, //Every Monday at 1:00
+			run() {
+				deleteCache();
+			},
+		}),
+	)
+	.get("/delete-cache", () => {
+		deleteCache();
+		return new Response("OK", { status: 200 });
+	})
+	.get("/", () => {
+		//Return 200 OK
+		return new Response("OK", { status: 200 });
+	})
+	.get("/*", renderImage)
+	.listen(environment().PORT);
 
 console.log(
-  `⚡🖼️ Pixtimize is running at ${app.server?.hostname}:${app.server?.port}`
+	`⚡🖼️ Pixtimize is running at ${app.server?.hostname}:${app.server?.port}`,
 );
--- src/services/deleteCache.ts
+++ src/services/deleteCache.ts
@@ -5,16 +5,20 @@ export const deleteCache = async () => {
 	console.log("Deleting cache");
 
 	try {
+		console.log("Deleting s3 folder");
 		//Delete s3 folder
 		await deleteFolder("cached/");
+		console.log("S3 folder deleted");
 	} catch (error) {
 		console.error("Error deleting cache", error);
 		throw new Error("Error deleting cache");
 	}
 
 	try {
+		console.log("Deleting redis cache");
 		//Delete keys in redis
 		await deleteKeys("cache:*");
+		console.log("Redis cache deleted");
 	} catch (error) {
 		console.error("Error deleting redis cache", error);
 		throw new Error("Error deleting redis cache");
